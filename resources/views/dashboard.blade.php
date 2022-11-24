6123f502e79da511386d4d74e7af1b09feee861b
rondi91 <rondie91@gmail.com>
2022-11-24 21:16:04 +0700

    membuat generate product

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,9 +8,19 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    You're logged in!
-                </div>
+                @foreach ($products as $product)
+                    
+                
+                    <div class="card text-center" style="width:18rem;">
+                      <img src="https://images.unsplash.com/photo-1561154464-82e9adf32764?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60" class="card-img-top" alt="...">
+                      <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <h6 class="card-subtitle mb-2 text-muted ">Card subtitle</h6>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        b5
+                      </div>
+                    </div>
+                    @endforeach
             </div>
         </div>
     </div>
